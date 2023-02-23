509ad23036aced23fef62cda6cbdf95569d463e6
nakamura-kaine <40160565+nakamura-kaine@users.noreply.github.com>
2023-02-23 15:10:06 +0900

    get,create関数アップデート
    get,create関数アップデート

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-#pathが通ってない時はパスを通してました。
 import sys; 
 sys.path.append('app')
 print(sys.path)
@@ -14,36 +13,54 @@ app.config["JSON_AS_ASCII"] = False
 app.register_blueprint(project.bp)
 
 #ルート、プロジェクト一覧表示
-#作成中
 @app.route('/')
 def project_get():
-    #dbDriverの生成とSQL実行
+    #dbDriverの生成
     regain_db_driver = dbDriver()
-    rows = regain_db_driver.sql_run("select project_id, project_name, DATE_FORMAT(estimated_time,'%k:%i') as estimated_time from projects")
-    regain_db_driver.db_close()
-    # return jsonify(rows)
     
-    return jsonify([
-            {
-                "project_id": 1,
-                "project_name": "プロジェクトA",
-                "estimated_time": "20:00",
-                "passed_time": "27:34"
-            },
-            {
-                "project_id": 2,
-                "project_name": "プロジェクトB",
-                "estimated_time": "10:00",
-                "passed_time": "3:10"
-            },
-            {
-                "project_id": 3,
-                "project_name": "プロジェクトC",
-                "estimated_time": "5:00",
-                "passed_time": "0:00"
-            }
-        ]
-    )
+    #プロジェクト一覧取得SQL
+    project_list_sql = f"""
+                            SELECT
+                                projects.project_id,
+                                project_name,
+                                DATE_FORMAT(projects.estimated_time,'%k:%i') as estimated_time,
+                                DATE_FORMAT(SEC_TO_TIME(IFNULL(SUM(TIME_TO_SEC(commit_time)), 0)),'%k:%i') as passed_time
+                            FROM
+                                projects
+                                LEFT JOIN processes
+                                    ON projects.project_id = processes.project_id
+                                LEFT JOIN tasks
+                                    ON processes.process_id = tasks.process_id
+                                LEFT JOIN commits
+                                    ON tasks.task_id = commits.task_id
+                            GROUP BY
+                                project_id
+                        """
+    rows = regain_db_driver.sql_run(project_list_sql)
+    regain_db_driver.db_close()
+
+    #dbDriverのクローズと値返却
+    regain_db_driver.db_close()
+    return jsonify(rows)
+
+#プロセスステータス名一覧
+@app.route('/process_statuses_name_list_get')
+def process_statuses_get():
+    #dbDriverの生成
+    regain_db_driver = dbDriver()
+    #プロセスステータス一覧取得SQL
+    name_list_sql = """
+                    SELECT
+                        status_id,
+                        status_name
+                    FROM
+                        process_statuses
+                    """
+    rows = regain_db_driver.sql_run(name_list_sql)
+
+    #dbDriverのクローズと値返却
+    regain_db_driver.db_close()
+    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run()
