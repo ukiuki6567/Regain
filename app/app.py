e696e0dbfe16c3449385ab13b83d4143e900eea9
kiyomi iida <areafriday@yahoo.co.jp>
2023-03-21 13:01:18 +0900

    tasks.htmlの動的対応中

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
+# flaskのファイル
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('menu.html') # templatesフォルダ内のindex.htmlを表示する
+    return render_template('tasks.html', task_name="jinja2のお勉強", status_name="完了" , priority_name="高", estimated_time="5:00" ,passed_time="3:00", deadline="2/28") # templatesフォルダ内のindex.htmlを表示する
 
 if __name__ == '__main__':
     app.run()
